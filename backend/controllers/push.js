65ecab79c43a9f337dafecabc517a2ab1c6e1b07
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-17 20:42:33 +0530

    push.js_completed

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -4,10 +4,28 @@ const {S3, S3_BUCKET} = require("../config/aws-config");
 
 async function pushFile(){
     const repoPath = path.resolve(process.cwd(), ".apnaGit");
-    const commitPath = path.join(repoPath, "commits");
+    const commitsPath = path.join(repoPath, "commits");
 
     try{
-        
+        const commitDirectories = await fs.readdir(commitsPath);
+        for(const commitDirectory of commitDirectories){
+            const commitPath = path.join(commitsPath, commitDirectory);
+            const files = await fs.readdir(commitPath);
+
+            for(const file of files){
+                const filePath = path.join(commitPath, file);
+                const fileContent = await fs.readFile(filePath);
+                const params = {
+                    Bucket: S3_BUCKET,
+                    key: `commits/${commitDirectories}/${file}`, 
+                    Body: fileContent,
+                };
+                
+                await S3.upload(params).promise();
+            };
+        };
+
+        console.log("All commits pushed to S3.");
     } catch (error) {
         console.error("Error; ", error);
     }
