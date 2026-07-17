7bcc3a170cfc826ac8228199ea0ca847b6540ddb
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-17 21:35:59 +0530

    pull.js_completed

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,38 @@
-function pullFile(){
-    console.log("Pull file is working");
+const fs = require("fs").promises;
+const path = require("path"); 
+const {S3, S3_BUCKET} = require("../config/aws-config");
+
+async function pullFile(){
+    const repoPath = path.resolve(process.cwd(), ".apnaGit");
+    const commitPath = path.join(repoPath, "commits");
+
+    try{
+        const data = await S3.listObjectsV2({
+            Bucket: S3_BUCKET, 
+            Prefix: "commits/",
+        }).promise();
+
+        const Objects = data.Contents;
+
+        for(const object of Objects){
+            const key = object.Key; 
+            const commitDirectory = path.join(commitPath, path.dirname(key).split("/").pop());
+
+            await fs.mkdir(commitDirectory, {recursive: true});
+
+            const params = {
+                Bucket: S3_BUCKET, 
+                Key: key,
+            };
+
+            const fileContent = await S3.getObject(params).promise();
+            await fs.writeFile(path.join(repoPath, key), fileContent);
+
+            console.log("All commits pulled from S3.");
+        };
+    } catch (error){
+        console.log("Error; ", error);
+    }
 };
 
 module.exports = { pullFile };
