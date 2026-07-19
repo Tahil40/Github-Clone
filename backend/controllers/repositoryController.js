451b87ba9f57293583140c4fac38e927aaac59f0
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-20 04:00:09 +0530

    configured_createRepository_functionality

--- backend/controllers/repositoryController.js
+++ backend/controllers/repositoryController.js
@@ -1,18 +1,97 @@
-const createRepository = (req, res) => {};
+const mongoose = require("mongoose");
+const repositoryModel = require("../models/repositoryModel");
+const userModel = require("../models/userModel");
+const issueModel = require("../models/issueModel");
 
-const getAllRepositories = (req, res) => {};
+const createRepository = async (req, res) => {
+  const { owner, name, content, description, visibility, issues } = req.body;
+  try {
+    if (!name) {
+      return res.status(400).json({ error: "Repository name is required!" });
+    }
 
-const fetchRepositoryById = (req, res) => {};
+    if (!mongoose.Types.ObjectId.isValid(owner)) {
+      return res.status(400).json({ error: "Invalid user ID!" });
+    }
 
-const fetchRepositoryByName = (req, res) => {};
+    if (!mongoose.Types.ObjectId.isValid(issues)) {
+      return res.status(400).json({ error: "Invalid user ID!" });
+    }
 
-const fetchRepositoryForLoggedInUser = (req, res) => {};
+    const newRepository = new repositoryModel({
+      name,
+      description,
+      content,
+      owner,
+      visibility,
+      issues,
+    });
 
-const updateRepositoryById = (req, res) => {};
+    const result = await newRepository.save();
+    res
+      .status(201)
+      .json({ message: "Repository created!", repositoryId: result._id });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  };
+};
 
-const deleteRepositoryById = (req, res) => {};
+const getAllRepositories = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
 
-const toggleVisibilityOfRepositoryById = (req, res) => {};
+const fetchRepositoryById = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const fetchRepositoryByName = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const fetchRepositoryForLoggedInUser = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const updateRepositoryById = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const deleteRepositoryById = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const toggleVisibilityOfRepositoryById = (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
+};
 
 module.exports = {
   createRepository,
