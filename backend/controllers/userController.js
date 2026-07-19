ba04b9b78032b093b2c883857151776898ea11e1
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-19 19:24:38 +0530

    configuring_login_functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,37 +24,62 @@ const signUP = async (req, res) => {
   try {
     await connectToMongoDB();
     const mongodb_db = client.db("GithubDatabase");
-    const user_collection = mongodb_db.collection("user");
+    const user_collection = mongodb_db.collection("User");
 
     const user = await user_collection.findOne({ username });
 
-    if(user){
-      return res.status(400).json({message: "user already exists!"});
-    }; 
+    if (user) {
+      return res.status(400).json({ message: "user already exists!" });
+    }
 
     const generate_salt = await bcrypt.genSalt(10);
     const hashed_password = await bcrypt.hash(password, generate_salt);
 
     const user_object = {
-      username, 
-      email, 
+      username,
+      email,
       password: hashed_password,
-      repositories: [], 
-      followedUser: [], 
-      starRepository: [], 
+      repositories: [],
+      followedUser: [],
+      starRepository: [],
     };
 
     const result = await user_collection.insertOne(user_object);
-    const token = jwt.sign({id: result.insertId}, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
     res.send(token);
   } catch (error) {
     console.error("Error; ", error);
     res.status(500).send("Server Error");
-  };
+  }
 };
 
-const login = (req, res) => {
-  
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    await connectToMongoDB();
+    const mongodb_db = client.db("GithubDatabase");
+    const user_collection = mongodb_db.collection("User");
+
+    const user = await user_collection.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const password_match = await bcrypt.compare(password, user.password);
+    if (!password_match) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token, userId: user._id });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("Server Error");
+  }
 };
 
 const getUserProfile = (req, res) => {};
